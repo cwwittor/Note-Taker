737d583364b45e70d66fbb1961d64f08793e0003
Christian Wittorp <cwwittor@ncsu.edu>
2021-04-28 01:46:11 -0400

    fixed picture

--- server.js
+++ server.js
@@ -6,17 +6,13 @@ const apiRoutes = require("./routes/apiRoutes.js");
 const app = express();
 const PORT = process.env.PORT || 3333;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-//app.use("/assets", express.static("./assets"));
 //app.use(express.urlencoded({ extended: true }));
+//app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "/public")));
+//app.use(express.static(path.join(__dirname, "/public")));
 
 htmlRoutes(app);
 apiRoutes(app);
-//app.use(apiRoutes);
-//app.use(htmlRoutes);
 
 app.listen(PORT, function () {
     console.log(`APP Listening on ${PORT}`);
